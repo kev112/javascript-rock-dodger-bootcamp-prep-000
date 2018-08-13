c80c80b8e737db5678513535e5e0fc80bab598d0
Tao Liu <taoliudesign@gmail.com>
2018-08-13 17:43:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -101,12 +101,19 @@ function moveDodger(e) {
 
 
    if (e.which == '37') {
+<<<<<<< HEAD
      e.preventDefault();
      e.stopPropagation();
      moveDodgerLeft();
    } else if (e.which == '39') {
      e.preventDefault();
      e.stopPropagation();
+=======
+     e.preventDefault()
+     moveDodgerLeft();
+   } else if (e.which == '39') {
+     e.preventDefault()
+>>>>>>> e459ff07919e9575697958e819003d82fc045676
      moveDodgerRight();
    }
 }
